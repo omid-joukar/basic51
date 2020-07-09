2454ef553adc941185fc0ca9f2d856c56ead83d7
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-09 16:14:59 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by KPS on 7/9/2020.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number(string): ");
+        String str1 = in.nextLine();
+        int result = Integer.parseInt(str1);
+        System.out.printf("The integer value is: %d",result);
+        System.out.printf("\n");
+    }
+}
+
